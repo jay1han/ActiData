1a2da04eb8cac751ed9e0734f9bdcd7fb94390c3
Jay Han <jayhan.name@gmail.com>
2024-05-01 16:50:11 +0900

    overwrite

--- actidata.py
+++ actidata.py
@@ -1,11 +1,6 @@
 import re, argparse, sys, os
 from datetime import datetime, timedelta
 
-# configuration
-expected_size = 5
-cycle_usec = 1000
-
-sample_rate = timedelta(microseconds=cycle_usec)
 STRPTIME = "%Y/%m/%d,%H:%M:%S.%f"
 STRFTIME = "%Y/%m/%d %H:%M:%S"
 STRFTIME2 = "%Y/%m/%d %H:%M:%S.%f"
@@ -18,13 +13,19 @@ parser.add_argument('-i', '--input',
                     help="Input file. Will use stdin if absent")
 parser.add_argument('-o', '--output',
                     help="Output file. Will use stdout if absent")
-parser.add_argument('-c', '--check', action='store_true',
+parser.add_argument('-c', '--check-only', action='store_true',
                     help="Only check, no output")
 parser.add_argument('-w', '--rewrite', action='store_true',
                     help="Rewrite input file. Meaningless if stdin is used")
 parser.add_argument('-a', '--analyze', action='store_true',
                     help="Analyze timestamps (slow)")
+parser.add_argument('-d', '--data-points', default=5,
+                    help="Number of data points (5)")
+parser.add_argument('-s', '--sampling-rate', default=1000,
+                    help="Sampling rate in microseconds (1000)")
 args = parser.parse_args()
+cycle_usec = args.sampling_rate
+sample_rate = timedelta(microseconds=cycle_usec)
 
 input = sys.stdin
 output = sys.stdout
@@ -39,10 +40,13 @@ else:
         output = open(tempfile, "w")
 
 if args.output is None:
-    print("Output: stdout", file=sys.stderr)
+    if args.rewrite:
+        print("Output: overwrite input file", file=sys.stderr)
+    else:
+        print("Output: stdout", file=sys.stderr)
 else:
     print(f"Output: {args.output}", file=sys.stderr)
-    if args.check or args.rewrite:
+    if args.check_only or args.rewrite:
         print("Contradictory options", file=sys.stderr)
         exit(1)
     output = open(args.output, "w")
@@ -55,7 +59,7 @@ prev_time = None
 start_time = None
 missing_time = timedelta(seconds=0)
 
-actidata = re.compile(f"([0-9/]+,[0-9:]+.[0-9]+)(,[-+0-9.]+){{{expected_size}}}")
+actidata = re.compile(f"([0-9/]+,[0-9:]+.[0-9]+)(,[-+0-9.]+){{{args.data_points}}}")
 for line in input:
     line = line.strip()
     input_lines += 1
@@ -69,7 +73,7 @@ for line in input:
         continue
     else:
         # datalist = map(float, data_str)
-        if not args.check:
+        if not args.check_only:
             print(line, file=output)
         output_lines += 1
 
@@ -78,16 +82,16 @@ for line in input:
             if prev_time is not None:
                 if sample_time - prev_time > sample_rate * cycle_usec:
                     missing = (sample_time - prev_time) // sample_rate - 1
-                    print(f"Line #{input_lines}: missing {missing} measurements ({missing / cycle_usec :.3f}s) at " +
+                    print(f"Line #{input_lines}: missing {missing} measurements ({missing / sample_rate :.3f}s) at " +
                           prev_time.strftime(STRFTIME2),
                           file=sys.stderr)
-                    missing_time += timedelta(microseconds=missing * cycle_usec)
+                    missing_time += timedelta(microseconds=missing * sample_rate)
             prev_time = sample_time
             if start_time is None: start_time = sample_time
 
-if input != sys.stdin:
+if input is not None and input != sys.stdin:
     input.close()
-if output != sys.stdout:
+if output is not None and output != sys.stdout:
     output.close()
 if args.rewrite:
     os.replace(tempfile, args.input)
@@ -103,15 +107,15 @@ print(f"Input {input_lines} lines, output {output_lines} lines" +
 if args.analyze:
     span_time = sample_time - start_time
     recorded_time = span_time - missing_time
-    recorded_cycles = (recorded_time.seconds * 1_000_000 + recorded_time.microseconds) / cycle_usec
-    processing_speed = recorded_cycles * cycle_usec / microseconds
+    recorded_cycles = (recorded_time.seconds * 1_000_000 + recorded_time.microseconds) / sample_rate
+    processing_speed = recorded_cycles * sample_rate / microseconds
     print(f"Speed {processing_speed :.1f}x real-time" +
           f" ({3600 / processing_speed :.1f}s/hour)",
           file=sys.stderr)
     print(f"Time span {start_time.strftime(STRFTIME)} to {sample_time.strftime(STRFTIME)}" +
           f" with {missing_time.seconds}s ({100.0 * missing_time.seconds / span_time.seconds:.3f}%) missing" +
           f" = {recorded_time.seconds}s recorded" +
-          f" -> {cycle_usec * output_lines / recorded_cycles :.2f}Hz",
+          f" -> {sample_rate * output_lines / recorded_cycles :.2f}Hz",
           file=sys.stderr)
 
 exit(error_lines)
